e4638454d2b5e0fb9ccbb890b22e7ed42763d104
Robert-W <robbie.winterbottom@gmail.com>
2024-10-02 23:14:15 -0400

    refactor: converted dfs to typescript

---
+++ src/algorithms/dfs/index.test.ts
@@ -0,0 +1,26 @@
+import { describe, test, expect } from 'bun:test';
+
+import DAG from '../../data-structures/dag';
+import dfs from './index';
+
+
+describe('Algorithm: Basic Depth First Search on a directed Graph', () => {
+
+	test('should return all child nodes from the root node', () => {
+		let graph = new DAG();
+
+		graph.add_edge('0', '1');
+		graph.add_edge('1', '2');
+		graph.add_edge('2', '3');
+		graph.add_edge('3', '4');
+		graph.add_edge('4', '5');
+		graph.add_edge('5', '6');
+
+		let expected = new Set(['0', '1', '2', '3', '4', '5', '6']);
+
+		expect(
+      dfs({ id: '0' }, graph)
+    ).toEqual(expected);
+	});
+
+});
--- src/algorithms/dfs/index.ts
+++ src/algorithms/dfs/index.ts
@@ -1,3 +1,9 @@
+import type DAG from "../../data-structures/dag";
+
+interface Vertex {
+  id: string;
+}
+
 /**
   * @function dfs
   * @description Function to initialize the dfs from
@@ -6,7 +12,7 @@
 	* @param {Set} visited - Set of nodes we have already visited
 	* @return {Set} set of nodes that are reachable via the original vertex
   */
-function dfs(vertex, graph, visited = new Set()) {
+function dfs(vertex: Vertex, graph: DAG, visited: Set<string> = new Set()): Set<string> {
   // Add our current vertex to the visited Set
   visited.add(vertex.id);
 
